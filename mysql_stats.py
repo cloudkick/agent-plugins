8c333440dcbeb77be33e37699918a1bf2abd2951
Geoff <geoff@cloudkick.com>
2010-07-21 17:24:25 -0700

    Add helpful info in mysql stats script.

--- mysql_stats.py
+++ mysql_stats.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+# By default, MySQL does not allow root to run mysqladmin with no password.
+# To get this script working, create /root/.my.cnf and put the following lines 
+# in it (sans # signs)...
+#
+#[client]
+#user = a_valid_mysql_user
+#password = a_secret_password_1234
+#host = localhost
+#
+# ...filling in your values for the user and password. Be sure to chmod .my.cnf
+# to 600. You don't want other users to be able to read the username and 
+# password.
+
 import commands
 import sys
 
