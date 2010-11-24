9b6862e50f57a0d0484bb56df750605157b850fa
Phil Kates <me@philkates.com>
2010-11-23 16:43:47 -0800

    Added MIT license and copyright

--- rds_stats.py
+++ rds_stats.py
@@ -2,8 +2,6 @@
 
 # Cloudkick plugin that monitors CloudWatch stats for RDS instances.
 #
-# Author: Phil Kates
-#
 # Requirements:
 # - Boto (http://boto.s3.amazonaws.com/index.html)
 #
@@ -11,6 +9,28 @@
 # 1. DBInstanceIdentifier
 # 2. AWS Access Key
 # 3. AWS Secret Access Key
+#
+# Author: Phil Kates
+# Copyright (c) 2010 Phil Kates <me@philkates.com>
+#
+# MIT License:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import datetime
 import sys
