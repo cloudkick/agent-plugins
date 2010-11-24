47a11181f6c4b744e67a9d7d9638ee0d53fe7a1b
Alex Polvi <polvi@cloudkick.com>
2010-11-23 18:11:08 -0800

    add some comments

--- turkey.py
+++ turkey.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
+#
+#                  _<_< > 
+#      ____________/_/ _/
+#    _/ turkey.py _/  /_
+#   / custom    /     \ >
+#  (   plugin   \_____//
+#   \________________/ 
+# 
+#  First used with a Backwoods smoker to monitor
+#  the temp of a turkey during cook time. You will
+#  need to adjust the temp thresholds to match your
+#  specific cooking requirements. Enjoy! 
+#
+#  ~Team Cloudkick, Thanksgiving 2010
+
 import time
 import struct
 import sys
@@ -6,26 +21,23 @@ import sys
 ldusb = file("/dev/ldusb0")
 
 time.sleep(0.5)
-#beep_temp = int(sys.argv[1])
 
-
-# for n in range(10):
-    # time.sleep(0.5)
+# This reads the payload off of the Go!Temp USB drive
 pkt = ldusb.read(8)
 parsed_pkt = list(struct.unpack("<BBHHH", pkt))
 num_samples = parsed_pkt.pop(0)
 seqno = parsed_pkt.pop(0)
 for sample in range(num_samples):
   c =  parsed_pkt[sample]/128.0
+
+  # Convert to Fahrenheit since this is for Thanksgiving
   f = 9.0 / 5.0 * c + 32
-#print "%2.2f C, %2.2f F" % (c, f)   
-#if f < beep_temp:
-#  print "\a",
-#    time.sleep(0.5)
+
+# This is the actual alerting threshold,
+# tweak as needed
 if f > 200 and f < 300:
   status = 'ok'
 else:
   status = 'err'
 print 'status %s temp at %d' % (status, f)
 print 'metric temp int %d' % (f)
-
