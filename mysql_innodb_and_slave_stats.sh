94f7868c3d3b446c8e2399b79680c85c96424954
C. Flores <flores@petalphile.com>
2010-12-06 22:37:47 +0000

    explicitly stating this script expects a cloudkick mysql user with minimal rights

--- mysql_innodb_and_slave_stats.sh
+++ mysql_innodb_and_slave_stats.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#just grabs innodb, lock tables, and slave lag for trending/graphing via cloudkick
+# just grabs innodb, lock tables, and slave lag for trending/graphing via cloudkick
+# expects a cloudkick user with the ability to show slave and global status
 
 echo "status ok ok"
 mysql -ucloudkick -e 'show global status' |ruby -a -ne 'puts "metric " + $F[0] + " int " + $F[1] if $_ =~ /[^b]lock|innodb/i'
