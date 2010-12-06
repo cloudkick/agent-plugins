920a7b9ac06c99a9884ea314a90f3f444c3fa756
C. Flores <flores@petalphile.com>
2010-12-06 23:35:01 +0000

    generic check that takes port to monitor as an argument.  if no listeners and/or clients, returns an error or critical, respectively

---
+++ any_connections.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Cloudkick plugin to count number of connections.  
+# Alerts if no clients and/or no listeners.
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <lo@petalphile.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return 
+# ----------------------------------------------------------------------------
+
+if [ $# -ne 1 ]; then
+	echo -e "usage `basename $0` <port to check>\nwhere <port to check> is 443 for https, 3306 for mysql, etc.\nalerts if no clients and/or listeners"
+	exit 1
+fi
+
+PORT=$1
+NOW=`netstat -ano |grep :$PORT`;
+SIMULTANEOUS=`echo "$NOW" |grep -c :$PORT`
+ACTIVE=`echo "$NOW" |grep -c ESTABLISHED`
+IDLE=`echo "$NOW" |grep -c TIME_WAIT`
+#remove the listener
+SIMULTANEOUS=$(($SIMULTANEOUS - 1))
+if [ $SIMULTANEOUS -lt 5 ]; then
+	if [ $SIMULTANEOUS -lt 0 ]; then
+		echo "status err no server listening!"
+		SIMULTANEOUS=0
+	else
+		echo "status critical no users connected!";
+	fi
+else
+	echo "status ok ok";
+fi
+echo "metric https_users int $SIMULTANEOUS";
+echo "metric https_users_active int $ACTIVE";
+echo "metric https_users_idle int $IDLE";
+
