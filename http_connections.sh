b17d63408bac8f6348d8f5dfefa84b8000de3bd3
C. Flores <flores@petalphile.com>
2010-12-06 23:35:49 +0000

    added phk's Beerware license for CloudKick pull request

--- http_connections.sh
+++ http_connections.sh
@@ -3,6 +3,14 @@
 # Cloudkick plugin to count number of http connections.  
 # Alerts if no clients and/or no listeners.
 
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <lo@petalphile.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return 
+# ----------------------------------------------------------------------------
+
+
 NOW=`netstat -ano |grep :80`;
 SIMULTANEOUS=`echo "$NOW" |grep -c :80`
 ACTIVE=`echo "$NOW" |grep -c ESTABLISHED`
--- mysql_innodb_and_slave_stats.sh
+++ mysql_innodb_and_slave_stats.sh
@@ -3,6 +3,14 @@
 # just grabs innodb, lock tables, and slave lag for trending/graphing via cloudkick
 # expects a cloudkick user with the ability to show slave and global status
 
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <lo@petalphile.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return 
+# ----------------------------------------------------------------------------
+
+
 echo "status ok ok"
 mysql -ucloudkick -e 'show global status' |ruby -a -ne 'puts "metric " + $F[0] + " int " + $F[1] if $_ =~ /[^b]lock|innodb/i'
 mysql -ucloudkick -e 'show slave status \G' |ruby -a -ne 'puts "metric slave_lag int " + $F[1] if $_ =~ /Seconds_Behind/'
