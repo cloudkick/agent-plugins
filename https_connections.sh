e09e5cfb2e145e9863d128fd65877e8bb6cb9140
C. Flores <flores@petalphile.com>
2010-12-06 22:33:26 +0000

    https check to count users and ensure there is a listener.

---
+++ https_connections.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Cloudkick plugin to count number of http connections.  
+# Alerts if no clients and/or no listeners.
+
+NOW=`netstat -ano |grep :443`;
+SIMULTANEOUS=`echo "$NOW" |grep -c :443`
+ACTIVE=`echo "$NOW" |grep -c ESTABLISHED`
+IDLE=`echo "$NOW" |grep -c TIME_WAIT`
+#remove the listener
+SIMULTANEOUS=$(($SIMULTANEOUS - 1))
+if [ $SIMULTANEOUS -lt 5 ]; then
+	if [ $SIMULTANEOUS -lt 0 ]; then
+		echo "status err no server listening!"
+	else
+		echo "status critical no users connected!";
+	fi
+else
+	echo "status ok ok";
+fi
+echo "metric https_users int $SIMULTANEOUS";
+echo "metric https_users_active int $ACTIVE";
+echo "metric https_users_idle int $IDLE";
+
