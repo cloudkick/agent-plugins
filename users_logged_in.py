7b553c8767a351a30cf5f27ecc225bb5d48f4e52
James Bair <james.d.bair@gmail.com>
2011-02-12 11:44:02 -0600

    Added --min and --max options. Also, it now properly limits the status string to 48 chars if desired.

--- users_logged_in.py
+++ users_logged_in.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python -tt
 # CloudKick Plugin to see who's logged into the system.
-# Alerts if anyone is logged in. Ideally used on production 
-# systems where no one should be logged in and if they are, 
-# you want to know about it.
+# 
+# By default, assumes that no one should be logged into the system.
+# 
+# Can specify --min and --max to adjust the acceptable range.
+# If number of users outside of the acceptable range, a warning
+# is raised. If inside the acceptable range, an ok status is raised.
 #
 # Copyright (C) 2011  James Bair <james.d.bair@gmail.com>
 #
@@ -23,13 +26,15 @@
 import commands
 import sys
 
+from optparse import OptionParser
+
 def getUsersLoggedIn():
     """
-    Find all users that are logged 
+    Find all users that are logged
     into the local system and return
     either a list or a None object.
     """
-    
+
     # Best way I can find to find users logged in
     commStatus, userData = commands.getstatusoutput('who')
 
@@ -37,7 +42,7 @@ def getUsersLoggedIn():
     if commStatus != 0:
         sys.stderr.write('status err Unable to execute "who" command.\n')
         sys.exit(1)
-    
+
     # "who" returns an empty string if no one is logged in
     if userData == '':
         users = None
@@ -52,28 +57,85 @@ def getUsersLoggedIn():
 
     return users
 
-# Let's do this
-if __name__ == '__main__':
+def main():
+    """
+    Main function for users_logged_in.py
+    """
+
+    minUsers=0
+    maxUsers=0
+
+    parser = OptionParser()
+
+    parser.add_option("-m", "--min",
+                      action="store", type="int",
+                      help="Minimum number of users", metavar="N")
+    parser.add_option("-M", "--max",
+                      action="store", type="int",
+                      help="Maximum number of users", metavar="N")
+
+    (options, args) = parser.parse_args()
+
+    # Overwrite default values if they exist
+    # Specify "not None" since 0 will return False
+    if options.min is not None:
+        minUsers = options.min
+    if options.max is not None:
+        maxUsers = options.max
+
+    # If only min users is passed, make them match.
+    # If both are passed, then this is a user error.
+    if minUsers > maxUsers:
+        if options.min is not None and options.max is not None:
+            msg = "Error: --min cannot exceed --max\n"
+            sys.stderr.write(msg)
+            sys.exit(1)
+        else:
+            maxUsers = minUsers
 
     # Find our users
     users = getUsersLoggedIn()
 
-    # If we have no users, we're done.
+    # If object is None, no users logged in.
     if users is None:
-        sys.stdout.write("status ok No users logged in.\n")
-        sys.stdout.write("metric users_logged_in int 0\n")
-        sys.exit(0)
+        userMsg="No users logged in."
+        userNum = 0
     # Anything else, we have users
     else:
-        # Find out if it's "user" or "users"
+        # Find out the number of u sers and
+        #if we're saying "user" or "users"
         userNum = len(users)
         if userNum == 1:
             userWord = 'user'
         else:
             userWord = 'users'
-        # Build our string and report the warning
-        sys.stdout.write("status warn %d %s logged in: " % (userNum, userWord))
-        sys.stdout.write(", ".join(users)[:48])
-        sys.stdout.write('\n')
-        sys.stdout.write("metric users_logged_in int %d\n" % (userNum,))
-        sys.exit(0)
+
+        # Build our users logged in message.
+        userMsg = "%d %s logged in: " % (userNum, userWord)
+        userMsg = userMsg + ", ".join(users)
+
+        # Plugin spec limits status string to 48 chars.
+        #
+        # I have tested with over 100 chars and CloudKick
+        # works just fine. Adjust/remove as you see fit, but
+        # for the sake of being "correct", this line is here.
+        userMsg = userMsg[:48]
+
+    # Now, find out if we are in error or not
+    if userNum < minUsers or userNum > maxUsers:
+        statusMsg = 'status warn'
+    else:
+        statusMsg = 'status ok'
+
+    # All done, build our strings and repot the data
+    ourStatus = '%s %s\n' % (statusMsg, userMsg)
+    # And build our number of users
+    ourMetric = "metric users_logged_in int %d\n" % (userNum,)
+
+    # All done.
+    sys.stdout.write(ourStatus + ourMetric)
+    sys.exit(0)
+
+# Let's do this
+if __name__ == '__main__':
+    main()
