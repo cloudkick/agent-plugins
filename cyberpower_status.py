51c96be917f7518541adc8c0cf2efcc1241a7e3b
James Bair <james.d.bair@gmail.com>
2011-09-29 11:11:47 -0500

    Cleaned up a bit.

--- cyberpower_status.py
+++ cyberpower_status.py
@@ -8,6 +8,8 @@ def getInfo():
    """
    Get all pertinent information from our CyberPower UPS
    """
+   
+   # Ensure pwrstat is present. If not, abort.
    status, out = commands.getstatusoutput('pwrstat -status')
    if status is not 0:
        msg = "Unable to find PowerPanel software.\n"
@@ -16,12 +18,15 @@ def getInfo():
        sys.stderr.write(msg)
        sys.exit(1)
 
+   # Build our results into key: value pairs.
    results = {}
+   # All values are separated by a line of periods. Get the items on either side.
    for line in out.split('\n'):
        if '.' in line:
            option = line.split('.')[0].strip()
            value = line.split('.')[-1].strip()
-           # Rename the following options and pull our integers as well.
+           # Start setting our numbers as integers.
+           # Rename the following options as well.
            if option == 'Load':
                option = 'Load Wattage'
                value = int(value.split()[0]) 
@@ -33,7 +38,7 @@ def getInfo():
            elif option == 'Battery Capacity':
                option = 'Battery Percentage'
                value = int(value.split()[0])
-           # The rest are all much easier. =)
+           # The rest are all much easier. =) Int only.
            elif option == 'Utility Voltage':
                value = int(value.split()[0])
            elif option == 'Output Voltage':
@@ -41,8 +46,10 @@ def getInfo():
            elif option == 'Rating Voltage':
                value = int(value.split()[0])
 
+           # Add our new key
            results[option] = value
-   
+
+      # Send the results
    return results
 
 def makeMetric(ourName, ourValue, gauge=False):
@@ -51,54 +58,57 @@ def makeMetric(ourName, ourValue, gauge=False):
     https://support.cloudkick.com/Creating_a_plugin
     """
 
+    # Check if string, int or float.
     if type(ourValue) is str:
         ourType = 'string'
     elif type(ourValue) is int:
         ourType = 'int'
+        # If an int, see if we need this as a gauge.
         if gauge is True:
             ourType = 'gauge'
     elif type(ourValue) is float:
         ourType = 'float'
+    # If not a supported type, abort.
     else:
         msg = 'Invalid value passed to makeMetric. Exiting.\n'
         sys.stderr.write(msg)
         sys.exit(1)
 
+    # Cannot have spaces in our name, so replace_with_underscores.
     ourName = ourName.replace(' ', '_')
 
+    # Send our metric.
     return 'metric %s %s %s\n' % (ourName, ourType, ourValue)
 
-# Do the damn thing.
+# MAIN
 if __name__ == '__main__':
+    # pwrstat requires root to poll UPS
     if os.getuid() != 0:
         msg = 'This script must be run as root.\n'
         sys.stderr.write(msg)
         sys.exit(1)
 
+    # Get our info from the UPS
     info = getInfo()
+    # If we have the info, check it's state
     if info:
         if info['State'] == 'Normal':
-            state = 'ok'
+            status = 'ok'
         else:
-            state = 'warn'
+            status = 'warn'
 
-        # First our status message
-        msg = "status %s Our %s UPS is in a '%s' state.\n" % (state, info['Model Name'], info['State'])
-        # Then, metrics.
-        # First, metrics where we rename the value
-        #msg += makeMetric('Load Wattage',  info['Load Wattage'])
-        #msg += makeMetric('Rating Wattage',  info['Rating Wattage'])
-        #msg += makeMetric('Battery Percentage', info['Battery Percentage'])
-        # And lastly, the boring ones.
-
-        # Maybe we should iterate like this:
-        for key, value in info.items():
-            msg += makeMetric(key, value)
-        # And have makeMetric decide if it's an int, float or a string.
+        # First, build our status message based on state.
+        msg = "status %s Our %s UPS is in a '%s' state.\n" % (status, info['Model Name'], info['State'])
+        
+        # Then, iterate through our keys
+        for k, v in info.items():
+            msg += makeMetric(k, v)
 
         # Send it all to stdout and we are done.
         sys.stdout.write(msg)
         sys.exit(0)
-    else:
-        print 'something went wrong.'
 
+    # If we got here, something went wrong.
+    else:
+        sys.stdout.write('status err Unable to get our UPS information.\n')
+        sys.exit(1)
